4defc893e138f297fd21dc6c9e35f643cadd3f7e
Gavin King <gavin.king@itslearning.com>
2016-07-03 17:02:11 +0200

    Working simple template

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,36 +1,53 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+
+let yeoman = require('yeoman-generator');
+let chalk  = require('chalk');
+let yosay  = require('yosay');
 
 module.exports = yeoman.Base.extend({
-  prompting: function () {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the fabulous ' + chalk.red('generator-plumpnation-basic-html') + ' generator!'
-    ));
+    prompting: function () {
+        let prompts = [{
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of the experiment?',
+            default: 'My experiment'
+        }];
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        // Have Yeoman greet the user.
+        this.log(yosay(
+            'Welcome to the fabulous ' +
+            chalk.red('generator-plumpnation-basic-html') +
+            ' generator!'
+        ));
 
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    }.bind(this));
-  },
+        return this.prompt(prompts)
+            .then((props) => {
+                // To access props later use this.props.someAnswer;
+                this.props = props;
+            });
+    },
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  },
+    writing: function () {
+        this.fs.copyTpl(
+            this.templatePath('index.html'),
+            this.destinationPath('index.html'),
+            {
+                title: this.props.title
+            }
+        );
 
-  install: function () {
-    this.installDependencies();
-  }
+        this.fs.copy(
+            this.templatePath('main.js'),
+            this.destinationPath('main.js')
+        );
+
+        this.fs.copy(
+            this.templatePath('styles.css'),
+            this.destinationPath('styles.css')
+        );
+    },
+
+    install: function () {
+        // this.installDependencies();
+    }
 });
--- test/app.js
+++ test/app.js
@@ -1,18 +1,21 @@
 'use strict';
-var path = require('path');
-var assert = require('yeoman-assert');
-var helpers = require('yeoman-test');
+
+let path    = require('path');
+let assert  = require('yeoman-assert');
+let helpers = require('yeoman-test');
 
 describe('generator-plumpnation-basic-html:app', function () {
-  before(function () {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
-      .toPromise();
-  });
+    before(function () {
+        return helpers.run(path.join(__dirname, '../generators/app'))
+            .withPrompts({title: 'Foobar'})
+            .toPromise();
+    });
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
-    ]);
-  });
+    it('creates files', function () {
+        assert.file([
+            'index.html',
+            'main.js',
+            'styles.css'
+        ]);
+    });
 });
